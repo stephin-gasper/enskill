67b089efb03da72f247237098e5116b271ed14ff
Stephin Gasper <stephin.gasper21@gmail.com>
2024-01-22 23:51:48 +0530

    All changes commit

---
+++ README.md
@@ -0,0 +1,47 @@
+# EnSkill
+
+> Official code for [enskill.in](https://enskill.in/) (discontinued) WordPress website
+
+[View Alternative Demo](https://enskill.000webhostapp.com/)
+
+## About
+
+This theme was created to build a WordPress website called [EnSkill](https://enskill.in/) for a business venture by a friend under the same name by me and was made live during 2016 but has been dicontinued for few years and was given permission to make the changes open source.
+
+The theme has many outdated code since it was created during 2016, before all the major changes to WordPress where introduced and not much code maintainence was done but it is still in workable condition.
+
+## Requirements
+
+* WordPress 4.0+
+* PHP 7.0+
+
+## Installation
+
+### Upload
+
+1. Download the latest tagged archive (choose the "zip" option).
+2. Go to the **Themes -> Add New Theme** screen and click the **Upload Theme** tab.
+3. Upload the zipped archive directly.
+4. Go to the Plugins screen and click **Activate**.
+
+### Manual
+
+1. Download the latest tagged archive (choose the "zip" option).
+2. Unzip the archive.
+3. Copy the folder to your `/wp-content/themes/` directory.
+4. Go to the Plugins screen and click **Activate**.
+
+## Local Development
+
+1. Set up a local WordPress development environment, you can use [Local](https://localwp.com/) or [wp-local-docker-v2](https://github.com/10up/wp-local-docker-v2)
+2. Ensure you are using WordPress 4.0+.
+3. Clone/download this repository into the `/wp-content/themes/` directory of your WordPress instance.
+4. In the WordPress admin, use the Appearance > Themes screen to activate EnSkill.
+
+## Contributors
+
+Stephin Gasper - [stephingasper.com](https://stephin-gasper.vercel.app/)
+
+## License
+
+This plugin is licensed under the GPLv2 License - see the [LICENSE](LICENSE) file for details.
---
+++ inc/template-tags.php
@@ -0,0 +1,119 @@
+<?php
+/**
+ * Custom template tags for this theme.
+ *
+ * Eventually, some of the functionality here could be replaced by core features.
+ *
+ * @package Enskill
+ */
+
+if ( ! function_exists( 'enskill_posted_on' ) ) :
+/**
+ * Prints HTML with meta information for the current post-date/time and author.
+ */
+function enskill_posted_on() {
+	$time_string = '<time class="entry-time" itemprop="datePublished" datetime="%1$s">%2$s</time>';
+
+	$time_string = sprintf( $time_string,
+		esc_attr( get_the_date( 'c' ) ),
+		esc_html( get_the_date() )
+	);
+
+	$posted_on = sprintf(
+		esc_html_x( 'Posted on %s', 'post date', 'enskill' ),
+		$time_string
+	);
+
+	$byline = sprintf(
+		esc_html_x( 'by %s', 'post author', 'enskill' ),
+		'<span class="entry-author" itemprop="author" itemscope="" itemtype="http://schema.org/Person"><a class="entry-author-link" itemprop="url" rel="author" href="' . esc_url( get_author_posts_url( get_the_author_meta( 'ID' ) ) ) . '"><span class="entry-author-name" itemprop="name">' . esc_html( get_the_author() ) . '</span></a></span>'
+	);
+
+	echo $posted_on . ' ' . $byline; // WPCS: XSS OK.
+
+}
+endif;
+
+if ( ! function_exists( 'enskill_entry_footer' ) ) :
+/**
+ * Prints HTML with meta information for the categories, tags and comments.
+ */
+function enskill_entry_footer() {
+    
+        echo '<p class="entry-meta">';
+	// Hide category and tag text for pages.
+	if ( 'post' === get_post_type() ) {
+		/* translators: used between list items, there is a space after the comma */
+		$categories_list = get_the_category_list( esc_html__( ', ', 'enskill' ) );
+		if ( $categories_list && enskill_categorized_blog() ) {
+			printf( '<span class="entry-categories">' . esc_html__( 'Posted in %1$s', 'enskill' ) . '</span>', $categories_list ); // WPCS: XSS OK.
+		}
+
+		/* translators: used between list items, there is a space after the comma */
+		$tags_list = get_the_tag_list( '', esc_html__( ', ', 'enskill' ) );
+		if ( $tags_list ) {
+			printf( '<span class="entry-tags">' . esc_html__( 'Tagged %1$s', 'enskill' ) . '</span>', $tags_list ); // WPCS: XSS OK.
+		}
+	}
+
+	if ( ! is_single() && ! post_password_required() && ( comments_open() || get_comments_number() ) ) {
+		echo '<span class="comments-link">';
+		comments_popup_link( esc_html__( 'Leave a comment', 'enskill' ), esc_html__( '1 Comment', 'enskill' ), esc_html__( '% Comments', 'enskill' ) );
+		echo '</span>';
+	}
+
+	edit_post_link(
+		sprintf(
+			/* translators: %s: Name of current post */
+			esc_html__( 'Edit %s', 'enskill' ),
+			the_title( '<span class="screen-reader-text">"', '"</span>', false )
+		),
+		'<span class="edit-link">',
+		'</span>'
+	);
+        echo '</p>';
+}
+endif;
+
+/**
+ * Returns true if a blog has more than 1 category.
+ *
+ * @return bool
+ */
+function enskill_categorized_blog() {
+	if ( false === ( $all_the_cool_cats = get_transient( 'enskill_categories' ) ) ) {
+		// Create an array of all the categories that are attached to posts.
+		$all_the_cool_cats = get_categories( array(
+			'fields'     => 'ids',
+			'hide_empty' => 1,
+			// We only need to know if there is more than one category.
+			'number'     => 2,
+		) );
+
+		// Count the number of categories that are attached to the posts.
+		$all_the_cool_cats = count( $all_the_cool_cats );
+
+		set_transient( 'enskill_categories', $all_the_cool_cats );
+	}
+
+	if ( $all_the_cool_cats > 1 ) {
+		// This blog has more than 1 category so enskill_categorized_blog should return true.
+		return true;
+	} else {
+		// This blog has only 1 category so enskill_categorized_blog should return false.
+		return false;
+	}
+}
+
+/**
+ * Flush out the transients used in enskill_categorized_blog.
+ */
+function enskill_category_transient_flusher() {
+	if ( defined( 'DOING_AUTOSAVE' ) && DOING_AUTOSAVE ) {
+		return;
+	}
+	// Like, beat it. Dig?
+	delete_transient( 'enskill_categories' );
+}
+add_action( 'edit_category', 'enskill_category_transient_flusher' );
+add_action( 'save_post',     'enskill_category_transient_flusher' );
